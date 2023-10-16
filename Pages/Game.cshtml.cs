fcd562c7b42c84c038d1af8b11e460679b9b4732
annayujeong <yujeongan@Yujeongs-MacBook-Pro.local>
2023-10-16 15:30:31 -0700

    Add game layout

---
+++ Pages/Game.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Covidiots.Pages
+{
+    public class GameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
