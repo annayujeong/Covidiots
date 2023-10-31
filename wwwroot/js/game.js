f8ed2844f26c7901a088621a3364ecf45ffbf6d8
SamuelT95 <samuel_tjahjadi@hotmail.com>
2023-10-31 15:14:17 -0700

    clean up code variable and function names, and added more constant strings and variables at the top for readability

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -19,28 +19,6 @@ function GameBoard(id) { this.id = id; }
 
 function Cell(id) { this.id = id; }
 
-function showProgressBar() {
-  progressBar.style.width = "0%"; // Reset the progress bar
-  progressBarContainer.style.display = "block"; // Show the progress bar container
-  isProgressBarActive = true;
-  let width = 0;
-  let intervalId = setInterval(() => {
-    if (width >= 100) { // Stop the interval when the progress bar reaches 100%
-      clearInterval(intervalId);
-      hideProgressBar();
-    } else {
-      width++; 
-      progressBar.style.width = width + '%'; // Increase the progress bar width by 1%
-    }
-  }, doorOpeningSpeed);
-}
-
-function hideProgressBar() {
-  progressBar.style.width = "0%";
-  progressBarContainer.style.display = "none"; // Hide the progress bar container
-  isProgressBarActive = false;
-}
-
 function initializeBoard(posX, posY) {
   for (let i = 0; i < MAX_TILES; i++) { // populate the board with tiles that can be either floor or wall or door
     let floor = document.createElement("div");
@@ -83,7 +61,6 @@ function placeItemRandomlyOnBoard() {
   item.className = items[randomItem];
 }
 
-
 function initializeProgressBar() { // Create the progress bar div and hides it by default
   progressBarContainer.id = "progress-bar-container";
   progressBarContainer.style.display = "none";
@@ -97,9 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let prevY = startingY;
   let destX = prevX;
   let destY = prevY;
-
   initializeBoard(prevX, prevY);
-
   document.addEventListener("keydown", function (event) {
     let key = event.key;
     if (key === "ArrowUp") {
@@ -140,16 +115,28 @@ document.addEventListener("DOMContentLoaded", function () {
       destX = prevX;
       destY = prevY;
     }
-    move(prevX, prevY, destX, destY);
+    switchCellClass(prevX, prevY, destX, destY);
     prevX = destX;
     prevY = destY;
   });
 });
 
-function move(prevX, prevY, destX, destY) {
-  array[prevX][prevY] = 0;
-  array[destX][destY] = 1;
-  switchCellClass(prevX, prevY, destX, destY);
+function showProgressBar() {
+  progressBar.style.width = "0%"; // Reset the progress bar
+  progressBarContainer.style.display = "block"; // Show the progress bar container
+  isProgressBarActive = true;
+  let width = 0;
+  let intervalId = setInterval(() => {
+    if (width >= 100) { // Stop the interval when the progress bar reaches 100%
+      clearInterval(intervalId); // Stop the interval
+      progressBar.style.width = "0%"; // Reset the progress bar
+      progressBarContainer.style.display = "none"; // Hide the progress bar container
+      isProgressBarActive = false;
+    } else {
+      width++; 
+      progressBar.style.width = width + '%'; // Increase the progress bar width by 1%
+    }
+  }, doorOpeningSpeed);
 }
 
 switchCellClass = (prevX, prevY, destX, destY) => { 
@@ -169,3 +156,6 @@ function isValidMovement(destX, destY) {
   }
   return true;
 }
+
+
+
