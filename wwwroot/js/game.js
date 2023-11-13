ccbeb46f5ea12af3d55e216a5f38868a7145e3d7
annayujeong <yujeongan@Yujeongs-MacBook-Pro.local>
2023-11-13 11:47:02 -0800

    Update code indentation

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -84,112 +84,114 @@ let isResource = false;
 let resourceBlock = null;
 
 document.addEventListener("DOMContentLoaded", function () {
-  connection.start().then(function ()
-  {
-    players = JSON.parse(document.getElementById("players").innerHTML.slice(0, -2));
-    let player = document.getElementById("user").innerHTML;
+	connection.start().then(function () {
+		players = JSON.parse(
+			document.getElementById("players").innerHTML.slice(0, -2)
+		);
+		let player = document.getElementById("user").innerHTML;
 
-    let prevX;
-    let prevY;
-    if(players[player] != null)
-    {
-      prevX = players[player].xPos;
-      prevY = players[player].yPos;
-    }
-    else
-    {
-      prevX = startingX;
-      prevY = startingY;
-    }
+		let prevX;
+		let prevY;
+		if (players[player] != null) {
+			prevX = players[player].xPos;
+			prevY = players[player].yPos;
+		} else {
+			prevX = startingX;
+			prevY = startingY;
+		}
 
-    let destX = prevX;
-    let destY = prevY;
-    console.log(prevX, prevY)
-    initializeBoard(prevX, prevY);
+		let destX = prevX;
+		let destY = prevY;
+		console.log(prevX, prevY);
+		initializeBoard(prevX, prevY);
 
+		for (let key in players) {
+			let value = players[key];
 
-    for(let key in players)
-    {
-      let value = players[key];
+			if (value.Email != player) {
+				document.getElementById(
+					rows * value.xPos + value.yPos
+				).className = "otherPlayers";
+			}
+		}
 
-      if(value.Email != player)
-      {
-        document.getElementById(rows * value.xPos + value.yPos).className = "otherPlayers";
-      }
-    }
+		document.addEventListener("keydown", function (event) {
+			let key = event.key;
+			if (key === "ArrowUp") {
+				key = "w";
+			}
+			if (key === "ArrowDown") {
+				key = "s";
+			}
+			if (key === "ArrowLeft") {
+				key = "a";
+			}
+			if (key === "ArrowRight") {
+				key = "d";
+			}
+			switch (key) {
+				case "w":
+					destX -= 1;
+					break;
+				case "s":
+					destX += 1;
+					break;
+				case "a":
+					destY -= 1;
+					break;
+				case "d":
+					destY += 1;
+					break;
+				default:
+					// Use number keys to use items
+					if (!isNaN(key) && key >= 1 && key <= 8) {
+						useItem(key);
+					}
+					break;
+			}
 
-    document.addEventListener("keydown", function (event) {
-      let key = event.key;
-      if (key === "ArrowUp") {
-        key = "w";
-      }
-      if (key === "ArrowDown") {
-        key = "s";
-      }
-      if (key === "ArrowLeft") {
-        key = "a";
-      }
-      if (key === "ArrowRight") {
-        key = "d";
-      }
-      switch (key) {
-        case "w":
-          destX -= 1;
-          break;
-        case "s":
-          destX += 1;
-          break;
-        case "a":
-          destY -= 1;
-          break;
-        case "d":
-          destY += 1;
-          break;
-        default:
-          // Use number keys to use items
-          if (!isNaN(key) && key >= 1 && key <= 8) {
-            useItem(key);
-          }
-          break;
-      }
- 
-      // Collect resource if it is resource block and spacebar is pressed
-      if (isResource && key === " ") {
-        collectResource();
-        return;
-      }
+			// Collect resource if it is resource block and spacebar is pressed
+			if (isResource && key === " ") {
+				collectResource();
+				return;
+			}
 
-      let destBlock = document.getElementById(rows * destX + destY);
-      if (destBlock.className === "door") {
-        if (!isProgressBarActive) {
-          showProgressBar();
-        }
-      }
+			let destBlock = document.getElementById(rows * destX + destY);
+			if (destBlock.className === "door") {
+				if (!isProgressBarActive) {
+					showProgressBar();
+				}
+			}
 
-      // Check if the dest block is resource
-      if (items.includes(destBlock.className)) {
-        isResource = true;
-        resourceBlock = destBlock;
-      } else {
-        isResource = false;
-        resourceBlock = null;
-      }
+			// Check if the dest block is resource
+			if (items.includes(destBlock.className)) {
+				isResource = true;
+				resourceBlock = destBlock;
+			} else {
+				isResource = false;
+				resourceBlock = null;
+			}
 
-      // Keep the previous position if false
-      if (isValidMovement(destX, destY) === false) {
-        destX = prevX;
-        destY = prevY;
-      }
-      switchCellClass(prevX, prevY, destX, destY);
-      prevX = destX;
-      prevY = destY;
-      connection.invoke("playerMove", player, destX.toString(), destY.toString()).catch(function (err) {
-        return console.error(err.toString());
-      });
-    
-    });
-
-  });
+			// Keep the previous position if false
+			if (isValidMovement(destX, destY) === false) {
+				destX = prevX;
+				destY = prevY;
+			}
+			switchCellClass(prevX, prevY, destX, destY);
+			prevX = destX;
+			prevY = destY;
+			connection
+				.invoke(
+					"playerMove",
+					player,
+					destX.toString(),
+					destY.toString()
+				)
+				.catch(function (err) {
+					return console.error(err.toString());
+				});
+		});
+	});
 });
 
 function collectResource() {
@@ -241,17 +243,18 @@ function isValidMovement(destX, destY) {
 	return true;
 }
 
-connection.on("playerMove", (playerName, x, y) =>
-{
-  console.log("playerMove");
-  let intX = parseInt(x);
-  let intY = parseInt(y);
-  switchCellClass(players[playerName].xPos, players[playerName].yPos, intX, intY);
-  players[playerName].xPos = intX;
-  players[playerName].yPos = intY;
-})
+connection.on("playerMove", (playerName, x, y) => {
+	console.log("playerMove");
+	let intX = parseInt(x);
+	let intY = parseInt(y);
+	switchCellClass(
+		players[playerName].xPos,
+		players[playerName].yPos,
+		intX,
+		intY
+	);
+	players[playerName].xPos = intX;
+	players[playerName].yPos = intY;
+});
 
-connection.on("update", () =>
-{
-
-})
+connection.on("update", () => {});
